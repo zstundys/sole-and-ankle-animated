c8adaf40f2063cb5046a5993661f126f93e196c2
Žanas Stundys <z.stundys@gmail.com>
2022-06-23 01:26:56 +0300

    Complete Exercise 1

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
-import Spacer from '../Spacer';
+import { QUERIES, WEIGHTS } from "../../constants";
+import { formatPrice, isNewShoe, pluralize } from "../../utils";
+import Spacer from "../Spacer";
 
 const ShoeCard = ({
   slug,
@@ -31,14 +30,21 @@ const ShoeCard = ({
       ? 'new-release'
       : 'default'
 
+  const hoverHue = {
+    "--hover-hue": `${parseInt(Math.random() * 360)}deg`,
+  };
+
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
-          <Image alt="" src={imageSrc} />
-          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
-          {variant === 'new-release' && (
-            <NewFlag>Just released!</NewFlag>
+          <ImageScaler style={hoverHue}>
+            <Image alt="" src={imageSrc} />
+          </ImageScaler>
+
+          {variant === "on-sale" && <SaleFlag style={hoverHue}>Sale</SaleFlag>}
+          {variant === "new-release" && (
+            <NewFlag style={hoverHue}>Just released!</NewFlag>
           )}
         </ImageWrapper>
         <Spacer size={12} />
@@ -46,20 +52,18 @@ const ShoeCard = ({
           <Name>{name}</Name>
           <Price
             style={{
-              '--color':
-                variant === 'on-sale'
-                  ? 'var(--color-gray-700)'
-                  : undefined,
-              '--text-decoration':
-                variant === 'on-sale' ? 'line-through' : undefined,
+              "--color":
+                variant === "on-sale" ? "var(--color-gray-700)" : undefined,
+              "--text-decoration":
+                variant === "on-sale" ? "line-through" : undefined,
             }}
           >
             {formatPrice(price)}
           </Price>
         </Row>
         <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-          {variant === 'on-sale' ? (
+          <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" ? (
             <SalePrice>{formatPrice(salePrice)}</SalePrice>
           ) : undefined}
         </Row>
@@ -75,13 +79,74 @@ const Link = styled.a`
 
 const Wrapper = styled.article``;
 
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  background: red;
+  height: 32px;
+  line-height: 32px;
+  padding: 0 10px;
+  font-size: ${14 / 18}rem;
+  font-weight: ${WEIGHTS.bold};
+  color: var(--color-white);
+  border-radius: 2px;
+`;
+
+const SaleFlag = styled(Flag)`
+  background-color: var(--color-primary);
+`;
+const NewFlag = styled(Flag)`
+  background-color: var(--color-secondary);
+`;
+
 const ImageWrapper = styled.div`
   position: relative;
+
+  @media ${QUERIES.prefersMotion} {
+    ${Flag} {
+      transition: background-color 300ms, border-radius 300ms;
+    }
+
+    &:hover ${Flag} {
+      background: hsl(var(--hover-hue) 100% 50%);
+      border-top-left-radius: 16px;
+      border-bottom-left-radius: 16px;
+    }
+  }
 `;
 
 const Image = styled.img`
   width: 100%;
+  display: block;
+`;
+
+const ImageScaler = styled.div`
   border-radius: 16px 16px 4px 4px;
+  overflow: hidden;
+
+  @media ${QUERIES.prefersMotion} {
+    --hover-hue: 0deg;
+
+    transition: box-shadow 500ms;
+
+    ${Image} {
+      transform-origin: 50% 85%; // Focus on the shoe
+      transition: transform 500ms, filter 500ms;
+      will-change: transform;
+    }
+
+    &:hover {
+      box-shadow: 0 0 0 5px hsl(var(--hover-hue) 100% 50%),
+        0 4px 24px -5px hsl(var(--hover-hue) 100% 50%);
+
+      ${Image} {
+        transform: scale(1.1);
+        transition-duration: 200ms;
+        filter: brightness(1.05);
+      }
+    }
+  } ;
 `;
 
 const Row = styled.div`
@@ -107,27 +172,6 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: var(--color-primary);
-`;
-
-const Flag = styled.div`
-  position: absolute;
-  top: 12px;
-  right: -4px;
-  background: red;
-  height: 32px;
-  line-height: 32px;
-  padding: 0 10px;
-  font-size: ${14 / 18}rem;
-  font-weight: ${WEIGHTS.bold};
-  color: var(--color-white);
-  border-radius: 2px;
-`;
-
-const SaleFlag = styled(Flag)`
-  background-color: var(--color-primary);
-`;
-const NewFlag = styled(Flag)`
-  background-color: var(--color-secondary);
 `;
 
 export default ShoeCard;
--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
 export const COLORS = {
-  white: '0deg 0% 100%',
+  white: "0deg 0% 100%",
   gray: {
-    100: '185deg 5% 95%',
-    300: '190deg 5% 80%',
-    500: '196deg 4% 60%',
-    700: '220deg 5% 40%',
-    900: '220deg 3% 20%',
+    100: "185deg 5% 95%",
+    300: "190deg 5% 80%",
+    500: "196deg 4% 60%",
+    700: "220deg 5% 40%",
+    900: "220deg 3% 20%",
   },
-  primary: '340deg 65% 47%',
-  secondary: '240deg 60% 63%',
+  primary: "340deg 65% 47%",
+  secondary: "240deg 60% 63%",
 };
 
 export const WEIGHTS = {
@@ -27,4 +27,5 @@ export const QUERIES = {
   phoneAndSmaller: `(max-width: ${BREAKPOINTS.phone / 16}rem)`,
   tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
   laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
+  prefersMotion: `(prefers-reduced-motion: no-preference)`,
 };
